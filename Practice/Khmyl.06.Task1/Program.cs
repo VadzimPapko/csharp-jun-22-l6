1b2a258e37ee6018cc1cc19ee5e1c778272be406
DaniilKhmyl <khmyldaniil@gmail.com>
2022-07-04 20:13:55 +0300

    LittleTipFromVS

--- Practice/Khmyl.06.Task1/Program.cs
+++ Practice/Khmyl.06.Task1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//наверное, не смог понять задание
+//наверное, не смог понять задание, 
 int[] arr = new int[0];
 
+//ну и визуалка подсказала
+int[] arr2 = Array.Empty<int>();
