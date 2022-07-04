2ef571f6eb19fb3c15ea9d292566d3c3e0b88c4d
Kate Konyahina <ekaterina.konyahina98@gmail.com>
2022-07-04 20:13:30 +0300

    Add Task2

---
+++ Practice/konyahina.06.Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+object[] arr = new object[3] {32, 'A', "Hey"};
+arr[0] =+ Int32.MaxValue;
+arr[2] = (string)arr[2] + " guys!";
+Console.Write(arr[0]);
+Console.Write(arr[1]);
+Console.Write(arr[2]);
