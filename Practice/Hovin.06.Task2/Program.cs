6f7b1215c5860e057720ed9917a674094337ad40
katherinaGo <govinkatherina@gmail.com>
2022-07-04 19:56:30 +0300

    Added task 2

---
+++ Practice/Hovin.06.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Task 2
+
+object[] array = new object[] { 32, 'A', "Hey" };
+string newString = " guys!";
+Console.WriteLine("Our array: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[0] = (int)array[0] + Int32.MaxValue;
+    array[2] = (string)array[2] + newString;
+    break;
+}
+
+Console.WriteLine("\nOur new array:");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
