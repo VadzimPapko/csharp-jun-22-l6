6d369452769cdb647a39b44980030255ac7e88ae
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-04 19:06:48 +0200

    add task2

---
+++ spurhiash.06.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] arr = new string[3];
+arr[0] = Convert.ToString(32);
+arr[1] = Convert.ToString('A');
+arr[2] = Convert.ToString("Hey");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i]);
+}
+
+Console.WriteLine();
+
+arr[0] = arr[0] + int.MaxValue;
+arr[2] = ($"{arr[2]} + guys");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
