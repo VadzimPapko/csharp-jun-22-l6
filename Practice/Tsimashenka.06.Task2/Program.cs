d30090632fb8cd26e25fbb5c376129a1cb7cf4c5
ValeryTsimashenka <shopat66@gmail.com>
2022-07-04 22:41:08 +0300

    remade + add task 1, 2, 3 (in task2 did not solve the problem)

---
+++ Practice/Tsimashenka.06.Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿object[] Array1 = new object[] { 32, 'A', "Hey"};
+
+int value = int.MaxValue;
+
+object[] Array2 = new object[] { value, ,"guys"};
+
+object[] newArray = new object[3];
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = Array1[i] + Array2[i];
+
+    Console.Write($"{newArray[i]} ");
+}
