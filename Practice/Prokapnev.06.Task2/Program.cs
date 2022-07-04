060c73dab93d9c330c64e4ee4988ac922705a37e
Anton_Prokapnev <antonprokapnev@gmail.com>
2022-07-04 22:24:04 +0300

    added task_02

---
+++ Practice/Prokapnev.06.Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿object[] array = new object[3] { 32, 'A', "Hey" };
+
+Console.Write("Was ");
+for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+Console.WriteLine();
+
+double tecInt = Convert.ToDouble(array[0]);
+
+array[0]=tecInt+Int32.MaxValue;
+
+array[2]+=" guys!";
+
+Console.Write("Became ");
+for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+Console.WriteLine();
