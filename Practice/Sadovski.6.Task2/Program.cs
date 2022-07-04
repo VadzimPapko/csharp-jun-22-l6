fa59891de5ab4ebe3d843c726c450f144adad6b6
VadzimSadovski <sadikvadik@icloud.com>
2022-07-04 20:14:56 +0300

    correct

--- Practice/Sadovski.6.Task2/Program.cs
+++ Practice/Sadovski.6.Task2/Program.cs
@@ -1,8 +1,9 @@
-﻿int n = 3;
-string[] mas1 = new string[n];
-mas1[0] = "32";
-mas1[1] = "A";
-mas1[2] = "Hey";
-
-Array.ForEach(mas1, x => Console.Write(x + " "));
+﻿Object[] obj()
+{
+    Object[] mas1 = new Object[3];
+    mas1[0] = 32;
+    mas1[1] = 'A';
+    mas1[2] = "Hey";
+    return mas1;
+}   
 Console.ReadKey();
