60320f92e4a2e8f684cb784af1eb9cfb1b4911e9
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-04 19:32:09 +0200

    add task3

---
+++ spurhiash.06.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int[133];
+Random random= new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(0,100);
+    Console.Write(arr[i]);
+}
+Console.WriteLine();
+int max = arr.Max();
+int index = Array.IndexOf(arr, max);
+Console.WriteLine($"Max number:{max}");
+Console.WriteLine($"Index : {index}");
+
+
+
+
+
