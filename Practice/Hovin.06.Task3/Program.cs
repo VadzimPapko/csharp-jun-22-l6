34ef3654caf3ab5f008665f329fa9891c96c0430
katherinaGo <govinkatherina@gmail.com>
2022-07-04 20:08:32 +0300

    added task 3

---
+++ Practice/Hovin.06.Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task 3
+
+Random random = new Random();
+int[] array = new int[133];
+int maxValue = default;
+int indexOfMax = default;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+
+maxValue = array.Max();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == array.Max())
+    {
+        indexOfMax = i;
+        break;
+    }
+}
+
+Console.WriteLine("\nMax value = "+ maxValue + " with index = " + indexOfMax);
