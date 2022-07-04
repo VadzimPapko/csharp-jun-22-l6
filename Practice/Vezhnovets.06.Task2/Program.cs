3d5fad358d7f5648a262734e4802d8ef9a0bb1c5
Euheni <eugene.vezhnavets@gmail>
2022-07-04 19:58:07 +0300

    Create second task

---
+++ Practice/Vezhnovets.06.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿object [] array = new object [3] {32, 'A', "Hey"};
+
+foreach (var item in array)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+
+int max = int.MaxValue;
+array[0] = 32 + max;
+array[2] = "Hey" + " guys!";
+
+foreach (var item in array)
+{
+    Console.Write(item + " ");
+}
