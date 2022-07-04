a8a5e83de2b8aff51413bf6705cfe6540e08775d
Euheni <eugene.vezhnavets@gmail>
2022-07-04 20:09:12 +0300

    Finish practice

--- Practice/Vezhnovets.06.Task3/Program.cs
+++ Practice/Vezhnovets.06.Task3/Program.cs
@@ -5,5 +5,14 @@ Random random = new Random();
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = random.Next(0, 100);
+}
+
+foreach (int i in array)
+{
     Console.Write(i + " ");
 }
+
+Console.WriteLine();
+
+int max = array.Max();
+Console.WriteLine($"Максимальный элемент равен: {max}");
