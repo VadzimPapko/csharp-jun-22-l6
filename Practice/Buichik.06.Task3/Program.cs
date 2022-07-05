64448d0d69a92bff4ca28f028891bd0e2c4fce2c
Sofi Buichik <sofi.buichik@yandex.by>
2022-07-05 17:31:25 +0300

    add solution, add task

---
+++ Practice/Buichik.06.Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] mas = new int[133];
+
+Random random = new Random(100);
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(0, 100);
+    Console.Write("{0}", mas[i]);
+}
+int max = mas.Max();
+Console.WriteLine("\nМаксимальное число {0}", max);
