5b2ed85e25808dba2666c74d08eeff48bfccbcac
Anatoly Aladko <desam1@mail.ru>
2022-07-05 23:27:06 +0300

    Add practice for lecture_06

---
+++ Practice/Aladko.06.Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create array
+
+object[] myObjArray = {32, 'A', "Hey"};
+
+foreach (var item in myObjArray)
+{
+    Console.Write(item + "\t");
+}
+Console.WriteLine("\n");
+
+myObjArray[0] = (int)myObjArray[0] + Int32.MaxValue;
+myObjArray[2] = myObjArray[2] + " guys!";
+
+foreach (var item in myObjArray)
+{
+    Console.Write(item + "\t");
+}
+Console.ReadKey();
---
+++ Practice/Aladko.06.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Max Element in Array
+
+int[] arrayRandom = new int[133];
+Random numberRandom = new Random();
+
+for (int i = 0; i < arrayRandom.Length; i++)
+{
+    arrayRandom[i] = numberRandom.Next(0, 100);
+    Console.Write(arrayRandom[i] + "\t");
+}
+int maxValue = arrayRandom.Max();
+int indexMax = Array.IndexOf(arrayRandom, maxValue);
+
+Console.WriteLine();
+Console.WriteLine("\nМаксимальный элемент в массиве равен " + maxValue + ", его индекс " + indexMax);
